41638cf03babe35506f867976bd2a5b42d95730b
aziztalyahya <aziz.t.alyahya@gmail.com>
2019-01-28 15:24:39 -0600

    finished doing queries within table

---
+++ select_exercises.sql
@@ -0,0 +1,9 @@
+use codeup_test_db;
+
+select * from albums where artist = 'Pink Floyd';
+select release_date from albums where name = 'Sgt.Peppers Lonely Hearts Club Band';
+select genre from albums where name = 'nevermind';
+select name from albums where release_date between '1990' and '1999';
+select name from albums where sales >= 20;
+select name from albums where genre = 'rock';
+
